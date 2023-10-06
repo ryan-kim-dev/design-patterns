fe59b2f3f22abaac3c82683d2c86b8ca34276fe1
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-10-06 13:51:28 +0900

    1-2: 레이아웃 컴포넌트의 기본 형태

--- layout-components/src/App.tsx
+++ layout-components/src/App.tsx
@@ -1,5 +1,15 @@
+import { SplitScreen } from '@/SplitScreen';
+
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: 'green' }}>Left Hand Component</h1>;
+};
+
+const RightHandComponent = () => {
+  return <p style={{ backgroundColor: 'lightgray' }}>Right Hand Component</p>;
+};
+
 function App() {
-  return <></>;
+  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
 }
 
 export default App;
